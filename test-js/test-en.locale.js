7fabeda4f6182ce7f3f06ceebae4cad2ce8e4dcc
= <=>
2024-05-22 14:38:31 +0300

    testing js import

---
+++ test-js/test-en.locale.js
@@ -0,0 +1,19 @@
+export const test2EnLocale = {
+  "decorators": {
+    "incorrectCredentials": "Incorrect credentials!"
+  },
+  "guards": {
+    "unauthorized": "Unauthorized!",
+    "incorrectCredentials": "Incorrect credentials!"
+  },
+  "dto": {
+    "test": {
+      "messageExample": "Message example!",
+      "somethingIsWrong": "Something is wrong!"
+    },
+    "test2": {
+      "message2Example": "Message 2 example!",
+      "somethingIsWrong": "Something is wrong 2!"
+    }
+  }
+}
---
+++ test-js/test-ua.locale.js
@@ -0,0 +1,19 @@
+export const test2UaLocale = {
+  decorators: {
+    incorrectCredentials: "Неправильні креденшиали!",
+  },
+  guards: {
+    unauthorized: "Неавторизовані!",
+    incorrectCredentials: "Неправильні креденшиали!",
+  },
+  dto: {
+    test: {
+      messageExample: "Приклад повідомлення!",
+      somethingIsWrong: "Щось не так!",
+    },
+    test3: {
+      messageExample: "Приклад повідомлення 3!",
+      somethingIsWrong: "Щось не так 3!",
+    },
+  },
+};
